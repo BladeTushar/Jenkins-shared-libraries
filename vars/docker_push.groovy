7d0af261ca592cba3e9454739ef6d34f015c73f3
Tushar Raj <104695344+BladeTushar@users.noreply.github.com>
2026-04-28 08:42:32 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,8 @@
-def call(String Project, String ImageTag, String dockerhubuser){
+def call(String ProjectName, String ImageTag, String dockerhubUser){
 withCredentials([usernamePassword(credentialsId: 'dockerhubcred', 
                                  usernameVariable: 'dockerhubUser', 
                                  passwordVariable: 'dockerhubPass')]){
                 sh "docker login -u ${dockerhubUser} -p ${dockerhubPass}"
-                sh "docker image tag notes-app:latest ${env.dockerhubUser}/notes-app:latest"
+                sh "docker image tag notes-app:latest ${dockerhubUser}/notes-app:latest"
                 sh "docker push ${dockerhubUser}/${ProjectName}:${ImageTag}"
 }
